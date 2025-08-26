fe4efd435b5a7b4b3d3c3304a00ed36b43faea75
jarvis0211 <jarvisbyharsh99@gmail.com>
2025-08-26 12:07:53 +0530

    Update sum_of_evenno.c

--- sum_of_evenno.c
+++ sum_of_evenno.c
@@ -3,14 +3,15 @@
 int main(){
 	int Number;
 	int i;
-	int Evennum =0;
+	int Evennum =2;
+	int sum =0;
 	printf("Enter the number of even number you want sum of :");
 	scanf("%d",&Number);
-	for (i=1;i<=Number+2;i++)
-	    if(i%2==0)
-		 Evennum = i+Evennum;
-         printf("%d",Evennum);
-	
+	for (i=2;i<=Number+2;i++)
+        sum +=Evennum;
+        Evennum +=2;
+        printf("%d",Evennum);
+		 
 return 0;
 	
 }
