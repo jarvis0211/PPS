50b45eb809c63e6f20953f7599fb93d82f399e56
jarvis0211 <jarvisbyharsh99@gmail.com>
2025-08-29 12:00:01 +0530

    Create factorial_in_recursion.c

---
+++ factorial_in_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    int res = 1, i;
+    for (i = 2; i <= n; i++)
+        res *= i;
+    return res;
+}
+
+int main()
+{
+    int num ;
+    printf("Enter the Number you want Factorial of :");
+    scanf("%d",&num);
+    printf("Factorial of %d is %d", num, factorial(num));
+    return 0;
+}
