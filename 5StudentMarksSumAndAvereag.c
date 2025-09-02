810dcc3d0b7e8d793e0fb5089b05e84b42b5fb54
jarvis0211 <jarvisbyharsh99@gmail.com>
2025-09-02 12:52:27 +0530

    Update 5StudentMarksSumAndAvereag.c

--- 5StudentMarksSumAndAvereag.c
+++ 5StudentMarksSumAndAvereag.c
@@ -2,20 +2,21 @@
 # include <stdio.h>
 
 int main(){
-	int Sub,Student,Sum=0,Marks;
+	int Sub,Student,Sum,Marks;
 	
 	for(Student=1;Student<=5;Student++){
-		
+		Sum=0;
 		printf("Marks of student %d \n",Student);
 		for(Sub=1;Sub<=3;Sub++){
 		 printf("\nEnter Marks for Subject %d:",Sub);
 		 scanf("%d",&Marks);
 		 Sum = Sum+Marks;	
 		}
+		printf("\nThe sum of Marks of students %d is : %d\n",Student,Sum);
+	    printf("The Average of Marks of Student %d is : %d\n",Student,Sum/5);
 		
 	}
-	printf("\nThe sum of all students is : %d\n",Sum);
-	printf("The Average is : %d\n",Sum/5);
+	
 	
 return 0;
 	
