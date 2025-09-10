566e21492333fc225a05fafa9db3ff14210c8f95
jarvis0211 <jarvisbyharsh99@gmail.com>
2025-09-10 11:46:53 +0530

    Create 10_9_25p1.c

---
+++ 10_9_25p1.c
@@ -0,0 +1,27 @@
+ //write a program to print the following patten
+  //   *
+ //   **
+ //  ***
+
+#include <stdio.h>
+
+int main() {
+    int rows;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        
+        for (int k = 1; k <= rows - i; k++) {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= i; j++) {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
