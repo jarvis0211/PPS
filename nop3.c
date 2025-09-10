ffae48c18489642df8bbb4ca5d8dd209dfbb88d6
jarvis0211 <jarvisbyharsh99@gmail.com>
2025-09-10 12:14:22 +0530

    Create nop3.c

---
+++ nop3.c
@@ -0,0 +1,29 @@
+ //write a program to print the following patten
+  //   *
+ //   **
+ //  ***
+
+#include <stdio.h>
+
+int main() {
+    int rows,a=0,b=rows-30;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = rows; i >= a-3; i--) {
+        
+        for (int k = 1; k <= rows - i; k++) {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= i; j++) {
+            printf("%d ",b);
+        }
+        printf("\n");
+        a++;
+        b--;
+    }
+
+    return 0;
+}
